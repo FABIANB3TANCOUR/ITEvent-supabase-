a49ec9845819ba167d5b2d239a69243f2813bc03
Davo11030 <rdfh1103@gmail.com>
2025-07-10 03:27:36 -0700

    fix: navigation bar para que funcione en todos los dispositivos

--- lib/screens/users/main_navigator.dart
+++ lib/screens/users/main_navigator.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
 import 'agenda.dart';
 import 'comunidad.dart';
 import 'eventos.dart';
@@ -7,25 +7,6 @@ import 'mensajes.dart';
 import 'notificaciones.dart';
 
 void navigateToPage(BuildContext context, int index) async {
-  // Solo necesitamos cargar el adminId si vamos a la pantalla de mensajes
-  if (index == 3) {
-    final prefs = await SharedPreferences.getInstance();
-    final adminId = prefs.getInt('adminId');
-    
-    if (adminId == null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('No se pudo identificar al administrador')),
-      );
-      return;
-    }
-
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (_) => const MensajesScreen()),
-    );
-    return;
-  }
-
   // Para las demás pantallas
   switch (index) {
     case 0:
@@ -44,6 +25,12 @@ void navigateToPage(BuildContext context, int index) async {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (_) => const ComunidadScreen()),
+      );
+      break;
+    case 3:
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const MensajesScreen()),
       );
       break;
     case 4:
